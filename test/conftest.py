b7a42de5a9722a7cb52b4a9677018f274568193b
Katrin Torsunova <katrin.tep@list.ru>
2023-09-24 19:48:16 +0300

    Изменена загрузка файла

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,11 @@
-import os
-from os import path
 import pytest
 from selene import browser
+from selene.support import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def brwsr_cnfg():
-
+    # driver = webdriver.Chrome('C:/Users/user/Desktop/chromedriver.exe')
     browser.config.window_width = 1900
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
